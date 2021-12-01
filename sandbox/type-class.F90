c261541556fb33d1aadc4677a3bdd4b399b664e6
Jeff Hammond <jeff.science@gmail.com>
2021-12-01 12:07:08 +0200

    more experiments

--- sandbox/type-class.F90
+++ sandbox/type-class.F90
@@ -1,23 +1,37 @@
 module d
 
     interface get
+#if defined(__PGI)
+        !module procedure get_type
+        module procedure get_class
+#endif
+#if defined(__INTEL_COMPILER)
+        !module procedure get_type
+        module procedure get_class
+#endif
+#if defined(__GNUC__)
         module procedure get_type
         !module procedure get_class
+#endif
     end interface
 
     contains
 
+#if defined(__GNUC__)
         function get_type(buffer) result(d)
             type(*), dimension(..), intent(in) :: buffer
             integer :: d
             d = 0
         end function
+#endif
 
+#if defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__PGI)
         function get_class(buffer) result(d)
             class(*), dimension(..), intent(in) :: buffer
             integer :: d
             d = 1
         end function
+#endif
 
 end module d
 
@@ -34,7 +48,11 @@ module m
 
         subroutine gb_bcast_standard(buffer,c)
             use d
+#if defined(__PGI) || defined(__INTEL_COMPILER)
+            class(*), dimension(..), intent(in) :: buffer
+#else
             type(*), dimension(..), intent(in) :: buffer
+#endif
             integer, intent(in) :: c
             print*,get(buffer),c
         end subroutine
